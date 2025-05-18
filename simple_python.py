5b903b1b82a459d8f2c4ad433307eda9a14ccc3c
Aabid Hussain <61867499+Ubaid1294@users.noreply.github.com>
2025-05-18 16:08:07 +0530

    Add files via upload

---
+++ simple_python.py
@@ -0,0 +1,17 @@
+print('Hello World')
+print(1+3)
+a = int(input('Enter a value = '))
+b = int(input('Enter another value = '))
+print(a+b)
+
+# Celsius ------ Farenheit
+C = int(input('Celsius = '))
+F = (C*9/5)+32
+print('Farenheit = ',F)
+
+
+
+# Farenheit ----- Celsius
+F = int(input('Farenheit = '))
+C = (F-32)*5/9
+print('Celsius = ',C)
