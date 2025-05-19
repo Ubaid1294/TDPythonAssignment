16a0f6b89cd78132db0f11da2ac9053655416909
Aabid Hussain <padderaabid18@gmail.com>
2025-05-19 23:03:46 +0530

    added control statement

--- ControlStructure.py
+++ ControlStructure.py
@@ -21,3 +21,64 @@ if Marks >= 40:
     print("You have Passed!")
 else:
     print("You have Failed!")
+
+
+#Challange
+Age = int(input('Enter Age: '))
+
+if Age >= 18:
+    print("You can vote.")
+else:
+    print("You can not vote.")
+
+
+# ELIF statement
+    ## if (Expression);
+    ##       Statement1
+    ## elif:
+    ##       Statement2
+    ## else:
+    ##       Statement3
+
+Operand_1 = int(input('Enter Operand 1: '))
+Operand_2 = int(input('Enter Operand 2: '))
+Operator = input('Enter Operator: (+,-,*,*)')
+
+if Operator == '+':
+    print(Operand_1 + Operand_2)
+elif Operator == '-':
+    print(Operand_1 - Operand_2)
+elif Operator == '*':
+    print(Operand_1 * Operand_2)
+elif Operator == '/':
+    print(Operand_1 / Operand_2)
+else:
+    print("Operator not recognized.")
+print('Thank You!')
+
+
+
+# range --- has three parameters start, end, steps
+
+n = list(range(11))
+print(n)
+n = list(range(3,11))
+print(n)
+
+n = list(range(2,11,2))
+print(n)
+
+
+# for loop
+
+l = ['Mike',10.2,1980]
+for n in l:
+    print(n)
+
+x= 'apple'
+for n in x:
+    print(n)
+
+
+for i in range(1,5):
+    print("n")
