0f0f20a41db2becbce8a59a7f65dbf92be5f15e5
Aabid Hussain <padderaabid18@gmail.com>
2025-05-19 21:12:44 +0530

    added the if statemrnt

---
+++ ControlStructure.py
@@ -0,0 +1,12 @@
+# IF statement
+    ## IF (Expression);
+    ##       Statement1
+    ## Statement2
+
+# a = 11
+# b = 20
+a = int(input('Enter a number: '))
+if a>5:
+    print("Greater than 5")
+print("None")
+
