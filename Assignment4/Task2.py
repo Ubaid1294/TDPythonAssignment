cf49ba021ba45ceba5b505b08650d2ea83d9bae1
Aabid Hussain <61867499+Ubaid1294@users.noreply.github.com>
2025-05-22 23:25:18 +0530

    Develop (#10)
    * update control statement
    
    * added the task1 and task2 of assignment2
    
    * updated the README.md and Task2.py
    
    * Added the assigment 3 task1 and updated the README.md for asignment3 task1
    
    * Added the assigment 3 task2
    
    * updated the README.md for assignment 3 task1 and task 2
    
    * updated README
    
    * updated the README.md for assignment 4 task1
    
    * updated the README.md for assignment 4 task2 and output.txt
    
    * updated the README.md for assignment 4 task2 and output.txt

---
+++ Assignment4/Task2.py
@@ -0,0 +1,19 @@
+# Task2 :Write and Append Data to a File
+# Problem Statement: Write a Python program that:
+# 1.Takes user input and writes it to a file named output.txt.
+# 2.Appends additional data to the same file.
+# 3.Reads and displays the final content of the file.
+
+InputWrite = input("Enter to write to the file: ")
+with open('output.txt','w') as file:
+    file.write(InputWrite +'\n')
+
+InputAppend = input("Enter additional text to append: ")
+with open('output.txt','a') as file:
+    file.write(InputAppend + '\n')
+
+print("Final content of output.txt:")
+
+with open('output.txt','r') as file:
+    ReadingFile = file.read()
+    print(ReadingFile)
