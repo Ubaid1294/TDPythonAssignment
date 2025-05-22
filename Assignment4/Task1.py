283798dc46b8da5281fb2b7bcb12c58dfab4f050
Aabid Hussain <padderaabid18@gmail.com>
2025-05-22 22:58:24 +0530

    updated the README.md for assignment 4 task1

---
+++ Assignment4/Task1.py
@@ -0,0 +1,14 @@
+# Task 1: Read a File and Handle Errors
+# Problem Statement:  Write a Python program that:
+# 1.Opens and reads a text file named sample.txt.
+# 2.Prints its content line by line.
+# 3.Handles errors gracefully if the file does not exist.
+
+try:
+    with open('sample.txt', 'r') as sample:
+        Line=1
+        for line in sample:
+            print("Line",Line,":",line.strip())
+            Line+=1
+except FileNotFoundError:
+    print("Error: The file 'sample.txt' was not found.")
