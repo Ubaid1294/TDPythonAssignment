69ef6b5643b41db6c61d5a7144995d84d99e0781
Aabid Hussain <padderaabid18@gmail.com>
2025-05-28 23:27:49 +0530

    added the learning notebooks

--- TKINTER/demo.py
+++ TKINTER/demo.py
@@ -2,9 +2,27 @@
 # Step 2: GUI interaction
 # Step 3: Adding inputs
 # Step 4: main loop
-
+# from tkinter import *
+# window = Tk()
+# inp = Label(window,text='Hello World!')
+# inp.pack()
+# window.mainloop()
 from tkinter import *
 window = Tk()
-inp = Label(window,text='Hello World')
-inp.pack()
-window.mainloop()
+window.title('Simple')
+window.geometry('500x600')
+window.configure(bg='green')
+frame1 = Frame(window,width=300,height=300,cursor= "dot")
+frame2 = Frame(window,width=300,height=300,cursor= "dotbox")
+button1 = Button(frame1,text="Button 1",bg='blue')
+button2 = Button(frame2,text="Button 2",bg='red')
+button3 = Button(frame1,text="Button 3",bg='yellow',fg='red')
+
+frame1.pack(side=TOP)
+frame2.pack(side=BOTTOM)
+button1.pack()
+button2.pack()
+button3.pack()
+
+
+mainloop()
