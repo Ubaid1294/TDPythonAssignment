8e5b311e8e74f72efad9fd09a9cf7cb23736b761
Aabid Hussain <padderaabid18@gmail.com>
2025-05-28 23:13:10 +0530

    added the learning notebooks

---
+++ TKINTER/demo.py
@@ -0,0 +1,10 @@
+# Step 1: import tkinter
+# Step 2: GUI interaction
+# Step 3: Adding inputs
+# Step 4: main loop
+
+from tkinter import *
+window = Tk()
+inp = Label(window,text='Hello World')
+inp.pack()
+window.mainloop()
